d5f64191a8bfc5d30a89c307f94dbf5b0c2e8e28
zkalykov <zhyrgalbekkalykov@gmail.com>
2023-11-16 01:40:25 -0600

    updatded cpp file

--- 2849. Determine if a Cell Is Reachable at a Given Time.cpp
+++ 2849. Determine if a Cell Is Reachable at a Given Time.cpp
@@ -1,29 +1,59 @@
-#include <iostream>
-#include <cstdlib>
+#include<bits/stdc++.h>
 using namespace std;
 
+
 class Solution {
-    public:
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        int n = adjacentPairs.size() + 1;
+        unordered_map<int, vector<int>> adjList;
+        
+        for (const auto& pair : adjacentPairs) {
+            adjList[pair[0]].push_back(pair[1]);
+            adjList[pair[1]].push_back(pair[0]);
+        }
+        
+        int start = 0;
+        for (const auto& entry : adjList) {
+            if (entry.second.size() == 1) {
+                start = entry.first;
+                break;
+            }
+        }
+        
+        vector<int> result;
+        map<int, int> seen;
+        dfs(adjList, start, seen, result);
+        
+        return result;
+    }
     
-    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        int xx = abs(fx - sx);
-        int yy = abs(fy - sy);
-        int cnt = 0;
-        if (xx < yy)
-            cnt = xx + (yy - xx);
-        else
-            cnt = yy + (xx - yy);
-        if (sx == fx && sy == fy)
-            return t != 1;
-        return t >= cnt;
+private:
+    void dfs(const unordered_map<int, vector<int>>& adjList, int current, map<int, int>& seen, vector<int>& result) {
+        result.push_back(current);
+        seen[current] = 1;
+        
+        for (auto neighbor : adjList.at(current) ) {
+            if (!seen[neighbor]) {
+                dfs(adjList, neighbor, seen, result);
+            }
+        }
     }
 };
 
 int main() {
-    Solution solution;
-    int sx=1,sy=1,fx=3,fy=3,t=4;
+    vector<vector<int>> test = {
+        {4,-2},
+        {1, 4},
+        {-3, 1}
+        
+    };
+    Solution obj;
     
-    cout<<(solution.isReachableAtTime(sx, sy, fx, fy, t)?"YES":"NO")
-
+    vector<int> sol;
+    sol = obj.restoreArray(test);
+    for (auto x : sol) {
+        cout << x << " ";
+    }
     return 0;
 }
